08b3c9906f6b54c7453c101762e1a90e525ebb8d
Unknown <joechegne@gmail.com>
2019-01-21 16:57:09 +0800

    3rd party libraries
    3rd party library implementation and home page setup.

--- core/init.php
+++ core/init.php
@@ -18,12 +18,12 @@ $GLOBALS['config'] = array(
 	)
 );
 
-//Autoload
+//Load all helper classes
 spl_autoload_register(function($class) {
-	require_once 'classes/' . $class . '.php';
+	require_once '../classes/' . $class . '.php';
 });
 
-require_once 'functions/sanitize.php';
+require_once '../functions/sanitize.php';
 
 if(Cookie::exists(Config::get('remember/cookie_name')) && !Session::exists(Config::get('session/session_name'))) {
 	$hash = Cookie::get(Config::get('remember/cookie_name')); //check session cookie hash
--- core/start.php
+++ core/start.php
@@ -1,11 +1,12 @@
 <?php
-require_once 'core/init.php';
+require_once 'init.php';
 
 ini_set('display_errors', 'On'); //PHP Configuration (For Errors locally)
 
-define('APP_ROOT', './App/'); //Root to "App" Directory
-define('VIEW_ROOT', APP_ROOT . '/views'); //Path to App/Views 
-define('BASE_URL', 'http://localhost/cms/' ); //Base Page
+define('MATH_ROOT', './'); //root of the entire directory
+define('APP_ROOT', '../app/'); //Root to "App" Directory
+define('VIEW_ROOT', APP_ROOT . 'views/page'); //Path to App/Views/page
+define('BASE_URL', 'http://localhost/ma+h/' ); //Base Page
 
 $db = new PDO('mysql:host=' . Config::get('mysql/host') . ';dbname=' . Config::get('mysql/db'), Config::get('mysql/username'), Config::get('mysql/password')); //will convert to use with helper class
 ?>
