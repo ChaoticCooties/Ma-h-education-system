93e8231ff5fe274adfb70538337f66ea39d627a3
Unknown <joechegne@gmail.com>
2019-01-28 15:38:15 +0800

    Basic Page Commit
    login/logout/proifle etc, next commit will complete integration

---
+++ functions/register.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<link rel="stylesheet" type="text/css" href="register.css">
+</head>
+<body>
+	<div id="container">
+		<p id="title">Create an account</p>
+		<form>
+			<p class="attr">EMAIL</p>
+			<input name="email" type="text" />
+			<br/>
+			<p class="attr">USERNAME</p>
+			<input name="username" type="text" />
+			<br/>
+			<p class="attr">PASSWORD</p>
+			<input name="password" type="text" />
+			<br/><br/><br/><br/><br/><br/>
+			<div id="button">
+				<input name="register" type="submit" value="Continue" />
+			</div>
+			<br/>
+			<a href="url">Already have an account?</a> 
+		</form>
+	</div>
+</body>
+</html>
