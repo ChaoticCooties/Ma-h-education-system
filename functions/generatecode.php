6b13cd3300cb35e05f6a9037499106f1b122c324
Chegne Eu Joe <joe@repsports.org>
2019-02-18 15:51:35 +0800

    Bug fix

--- functions/generatecode.php
+++ functions/generatecode.php
@@ -7,6 +7,6 @@ $classID = $user->idToClassID($user->data()->id);
 
 
 echo "Your invite link is ";
-echo BASE_URL . "functions/joinclass.php?class=" . $user->getClassCode($classID) . "";
+echo BASE_URL . "functions/joinclass.php?class=" . '"' . $user->getClassCode($classID) . '"';
 
 ?>
